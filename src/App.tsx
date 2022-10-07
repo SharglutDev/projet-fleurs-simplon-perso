85d652b16b2365b39a4a7a705219b108ca3dc8bf
sharglut <sharglut@free.fr>
2022-10-07 16:25:17 +0200

    install bootstrap + integration navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
+  const message = "Hello World";
+
+  const handleClick = (name: string) => {
+    alert(`Bonjour ${name}`);
+  };
+
+  const handleClickSansParem = () => {
+    alert(`Appel static`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <p className="color-pink">{message}</p>
+      <button className="btn btn-success" onClick={() => handleClick("Joseph")}>
+        Test Joseph
+      </button>
+      <p>Coucou</p>
+      <button className="btn btn-primary" onClick={handleClickSansParem}>
+        Test
+      </button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg bg-info shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Société Nature Cueillette Fleurs
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Details
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
